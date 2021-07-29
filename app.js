b2bedb19a5b356f47395953a8fad3e4fcc06b3d1
Gabriel Novais <gabriel_novais1997@hotmail.com>
2021-07-29 19:53:42 -0300

    Meio aula

--- app.js
+++ app.js
@@ -10,4 +10,30 @@ function par(number) {
 
 var simPar = par(20);
 
-console.log(simPar);
+function soma(num1, num2) {
+  return num1 + num2;
+}
+
+// let resultSoma = soma(13, 88);
+
+// if (resultSoma > 100) {
+//   console.log('Maior do que cem.');
+// } else {
+//   console.log('Menor ou igual a cem.');
+// }
+
+/*
+ * Math.round(x)   Returns x rounded to its nearest integer
+ * Math.ceil(x)	   Returns x rounded up to its nearest integer
+ * Math.floor(x)	Returns x rounded down to its nearest integer
+ * Math.trunc(x)   Returns the integer part of x (new in ES6)
+ * Math.sign(x)    Returns if x is negative, null or positive
+ */
+function arredonda(number) {
+  return Math.floor(number);
+}
+// console.log(arredonda(7.9));
+
+const somaArrow
+
+
