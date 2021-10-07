f047ebe797f55188d76b5b8538dfc239f8e63b72
Gabriel Novais <gabriel_novais1997@hotmail.com>
2021-10-07 19:59:26 -0300

    Login realizado

---
+++ techsoft/app/Http/Controllers/SistemaController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SistemaController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        return view('sistema.dashboard.index');
+    }
+
+
+}
