047b9e2f624e64200fa92e917f2671a0a614ed19
Gabriel Novais <gabriel_novais1997@hotmail.com>
2021-10-19 20:24:08 -0300

    Form cadastrar produto

--- techsoft/resources/views/sistema/products/create.blade.php
+++ techsoft/resources/views/sistema/products/create.blade.php
@@ -21,4 +21,80 @@
         </div>
     </div>
 </div>
+
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card card-primary">
+                    <div class="card-header">
+                        <h3 class="card-title" id="title">Cadastrando</h3>
+                    </div>
+                    <form id="save" enctype="multipart/form-data">
+                        @csrf
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <div class="form-group">
+                                        <label for="name">Título</label>
+                                        <input type="text" class="form-control" name="title" id="title_input"
+                                            placeholder="Título">
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-4">
+                                    <div class="form-group">
+                                        <label for="name">Preço</label>
+                                        <input type="number" class="form-control" name="price" id="price_input"
+                                            placeholder="Preço">
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-4">
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" name="active" id="active_input">
+                                        <label class="form-check-label" for="active_input">Ativo/Desativo</label>
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label for="name">Imagem</label>
+                                        <input type="file" class="form-control" name="image" id="image_input">
+                                        <img id="product_image" src="" width="200">
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label for="name">Categoria</label>
+                                        <select id="category_input" class="form-control" name="category_id">
+                                            @foreach($categories as $category)
+                                            <option value="{{ $category->id }}">
+                                                {{ $category->title }}
+                                            </option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-12">
+                                    <div class="form-group">
+                                        <label for="name">Descrição</label>
+                                        <textarea id="description_input" class="form-control"
+                                            name="description"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-primary">Salvar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
